e0090f38a42e9d313afe2d9ed72dc231d877fd97
Yusuke Sangenya <longinus.eva@gmail.com>
2020-09-13 14:11:55 +0900

    hoge

--- linux/src/main.rs
+++ linux/src/main.rs
@@ -10,28 +10,32 @@ use std::io::Write;
 
 use std::process::Command;
 
-fn fetch_config() -> Option<mapper::config::Config> {
+fn fetch_config() -> mapper::config::Config {
   let yaml = load_yaml!("cli.yml");
   let matches = App::from_yaml(yaml).get_matches();
 
   if let Some(xremap_config_fname) = matches.value_of("xremap_config") {
-    let mut converter_rb = tempfile::NamedTempFile::new().ok()?;
+    let mut converter_rb = tempfile::NamedTempFile::new().unwrap();
     let converter_rb_source = include_str!("convert.rb");
     converter_rb
       .write_all(converter_rb_source.as_bytes())
-      .ok()?;
+      .unwrap();
     let output = Command::new("ruby")
-      .args(vec![converter_rb.path().to_str()?, xremap_config_fname])
+      .args(vec![
+        converter_rb.path().to_str().unwrap(),
+        xremap_config_fname,
+      ])
       .output()
-      .ok()?;
-    return serde_json::from_reader::<&[u8], mapper::config::Config>(output.stdout.as_ref()).ok();
+      .unwrap();
+    return serde_json::from_reader::<&[u8], mapper::config::Config>(output.stdout.as_ref())
+      .unwrap();
   }
 
   if let Some(config_fname) = matches.value_of("config") {
-    return serde_json::from_reader(std::fs::File::open(config_fname).ok()?).ok();
+    return serde_json::from_reader(std::fs::File::open(config_fname).unwrap()).unwrap();
   }
 
-  None
+  panic!("xremap_config or config option must be specified!")
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -44,7 +48,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
   trace!("start");
 
-  let config = fetch_config().unwrap();
+  let config = fetch_config();
   let parser = linux::config::XParser::build(&linux::config::XIntoDomain);
   let key_bind_for_focus = parser.build_keybind_for_focus(config.clone())?;
   let possible_keyinput_finder = parser.build_possible_keyinput_finder(config.clone())?;
